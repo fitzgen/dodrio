c0017ed98b2d09786f855d3325489cf5815187c7
Nick Fitzgerald <fitzgen@gmail.com>
2019-01-30 09:57:48 -0800

    Let the compile-tests crate build for non-test builds

--- compile-tests/src/main.rs
+++ compile-tests/src/main.rs
@@ -1,24 +1,26 @@
-#![cfg(test)]
+fn main() {}
 
-extern crate compiletest_rs as compiletest;
+#[cfg(test)]
+mod tests {
+    extern crate compiletest_rs as compiletest;
+    use std::path::PathBuf;
 
-use std::path::PathBuf;
+    fn run_mode(mode: &'static str) {
+        let mut config = self::compiletest::Config::default();
 
-fn run_mode(mode: &'static str) {
-    let mut config = compiletest::Config::default();
+        config.mode = mode.parse().expect("Invalid mode");
+        config.src_base = PathBuf::from(format!("{}", mode));
 
-    config.mode = mode.parse().expect("Invalid mode");
-    config.src_base = PathBuf::from(format!("{}", mode));
+        // Populate config.target_rustcflags with dependencies on the path.
+        config.link_deps();
+        // If your tests import the parent crate, this helps with E0464
+        config.clean_rmeta();
 
-    // Populate config.target_rustcflags with dependencies on the path.
-    config.link_deps();
-    // If your tests import the parent crate, this helps with E0464
-    config.clean_rmeta();
+        compiletest::run_tests(&config);
+    }
 
-    compiletest::run_tests(&config);
-}
-
-#[test]
-fn compile_test() {
-    run_mode("compile-fail");
+    #[test]
+    fn compile_test() {
+        run_mode("compile-fail");
+    }
 }
