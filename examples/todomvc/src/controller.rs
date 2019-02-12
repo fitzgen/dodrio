24743b93f0d6de503dd0afcca0acfdc6c1b3a57f
Nick Fitzgerald <fitzgen@gmail.com>
2019-02-12 10:59:10 -0800

    TodoMVC: Use an RAII type to auto render and save to local storage
    Instead of a `with<F>`-style function. It is a bit more idiomatic, and provides
    nicer usage, but takes a little more to write.

--- examples/todomvc/src/controller.rs
+++ examples/todomvc/src/controller.rs
@@ -6,6 +6,7 @@ use crate::todos::{Todos, TodosActions};
 use crate::visibility::Visibility;
 use dodrio::{RootRender, VdomWeak};
 use serde::{Deserialize, Serialize};
+use std::ops::{Deref, DerefMut};
 
 /// The controller for the TodoMVC app.
 ///
@@ -18,107 +19,122 @@ use serde::{Deserialize, Serialize};
 #[derive(Default, Deserialize, Serialize)]
 pub struct Controller;
 
-/// Invoke `f` with the root `Todos` component, save the (likely just modified)
-/// todos to local storage, and schedule a new `dodrio` render.
-fn with_todos<F>(root: &mut dyn RootRender, vdom: VdomWeak, f: F)
-where
-    F: FnOnce(&mut Todos),
-{
-    let todos = root.unwrap_mut::<Todos>();
-    f(todos);
-    todos.save_to_local_storage();
-    vdom.schedule_render();
-}
-
 impl TodosActions for Controller {
     fn toggle_all(root: &mut dyn RootRender, vdom: VdomWeak) {
-        with_todos(root, vdom, |todos| {
-            let all_complete = todos.todos().iter().all(|t| t.is_complete());
-            for t in todos.todos_mut() {
-                t.set_complete(!all_complete);
-            }
-        });
+        let mut todos = AutoCommitTodos::new(root, vdom);
+        let all_complete = todos.todos().iter().all(|t| t.is_complete());
+        for t in todos.todos_mut() {
+            t.set_complete(!all_complete);
+        }
     }
 
     fn update_draft(root: &mut dyn RootRender, vdom: VdomWeak, draft: String) {
-        with_todos(root, vdom, |todos| {
-            todos.set_draft(draft);
-        });
+        let mut todos = AutoCommitTodos::new(root, vdom);
+        todos.set_draft(draft);
     }
 
     fn finish_draft(root: &mut dyn RootRender, vdom: VdomWeak) {
-        with_todos(root, vdom, |todos| {
-            let title = todos.take_draft();
-            let title = title.trim();
-            if !title.is_empty() {
-                let id = todos.todos().len();
-                let new = Todo::new(id, title);
-                todos.add_todo(new);
-            }
-        });
+        let mut todos = AutoCommitTodos::new(root, vdom);
+        let title = todos.take_draft();
+        let title = title.trim();
+        if !title.is_empty() {
+            let id = todos.todos().len();
+            let new = Todo::new(id, title);
+            todos.add_todo(new);
+        }
     }
 
     fn change_visibility(root: &mut dyn RootRender, vdom: VdomWeak, vis: Visibility) {
-        with_todos(root, vdom, |todos| {
-            todos.set_visibility(vis);
-        });
+        let mut todos = AutoCommitTodos::new(root, vdom);
+        todos.set_visibility(vis);
     }
 
     fn delete_completed(root: &mut dyn RootRender, vdom: VdomWeak) {
-        with_todos(root, vdom, |todos| {
-            todos.delete_completed();
-        });
+        let mut todos = AutoCommitTodos::new(root, vdom);
+        todos.delete_completed();
     }
 }
 
 impl TodoActions for Controller {
     fn toggle_completed(root: &mut dyn RootRender, vdom: VdomWeak, id: usize) {
-        with_todos(root, vdom, |todos| {
-            let t = &mut todos.todos_mut()[id];
-            let completed = t.is_complete();
-            t.set_complete(!completed);
-        });
+        let mut todos = AutoCommitTodos::new(root, vdom);
+        let t = &mut todos.todos_mut()[id];
+        let completed = t.is_complete();
+        t.set_complete(!completed);
     }
 
     fn delete(root: &mut dyn RootRender, vdom: VdomWeak, id: usize) {
-        with_todos(root, vdom, |todos| {
-            todos.delete_todo(id);
-        });
+        let mut todos = AutoCommitTodos::new(root, vdom);
+        todos.delete_todo(id);
     }
 
     fn begin_editing(root: &mut dyn RootRender, vdom: VdomWeak, id: usize) {
-        with_todos(root, vdom, |todos| {
-            let t = &mut todos.todos_mut()[id];
-            let desc = t.title().to_string();
-            t.set_edits(Some(desc));
-        });
+        let mut todos = AutoCommitTodos::new(root, vdom);
+        let t = &mut todos.todos_mut()[id];
+        let desc = t.title().to_string();
+        t.set_edits(Some(desc));
     }
 
     fn update_edits(root: &mut dyn RootRender, vdom: VdomWeak, id: usize, edits: String) {
-        with_todos(root, vdom, |todos| {
-            let t = &mut todos.todos_mut()[id];
-            t.set_edits(Some(edits));
-        });
+        let mut todos = AutoCommitTodos::new(root, vdom);
+        let t = &mut todos.todos_mut()[id];
+        t.set_edits(Some(edits));
     }
 
     fn finish_edits(root: &mut dyn RootRender, vdom: VdomWeak, id: usize) {
-        with_todos(root, vdom, |todos| {
-            let t = &mut todos.todos_mut()[id];
-            if let Some(edits) = t.take_edits() {
-                let edits = edits.trim();
-                if edits.is_empty() {
-                    todos.delete_todo(id);
-                } else {
-                    t.set_title(edits);
-                }
+        let mut todos = AutoCommitTodos::new(root, vdom);
+        let t = &mut todos.todos_mut()[id];
+        if let Some(edits) = t.take_edits() {
+            let edits = edits.trim();
+            if edits.is_empty() {
+                todos.delete_todo(id);
+            } else {
+                t.set_title(edits);
             }
-        });
+        }
     }
 
     fn cancel_edits(root: &mut dyn RootRender, vdom: VdomWeak, id: usize) {
-        with_todos(root, vdom, |todos| {
-            let t = &mut todos.todos_mut()[id];
-            let _ = t.take_edits();
-        });
+        let mut todos = AutoCommitTodos::new(root, vdom);
+        let t = &mut todos.todos_mut()[id];
+        let _ = t.take_edits();
+    }
+}
+
+/// An RAII type that dereferences to a `Todos` and once it is dropped, saves
+/// the (presumably just modified) todos to local storage, and schedules a new
+/// `dodrio` render.
+pub struct AutoCommitTodos<'a> {
+    todos: &'a mut Todos,
+    vdom: VdomWeak,
+}
+
+impl AutoCommitTodos<'_> {
+    /// Construct a new `AutoCommitTodos` from the root rendering component and
+    /// `vdom` handle.
+    pub fn new(root: &mut dyn RootRender, vdom: VdomWeak) -> AutoCommitTodos {
+        let todos = root.unwrap_mut::<Todos>();
+        AutoCommitTodos { todos, vdom }
+    }
+}
+
+impl Drop for AutoCommitTodos<'_> {
+    fn drop(&mut self) {
+        self.todos.save_to_local_storage();
+        self.vdom.schedule_render();
+    }
+}
+
+impl Deref for AutoCommitTodos<'_> {
+    type Target = Todos;
+
+    fn deref(&self) -> &Todos {
+        &self.todos
+    }
+}
+
+impl DerefMut for AutoCommitTodos<'_> {
+    fn deref_mut(&mut self) -> &mut Todos {
+        &mut self.todos
     }
 }
